231ba235299c936459e4ecc779f4fc010274d168
Syed Shah <tahashah2005n@gmail.com>
2025-09-07 21:29:35 -0400

    Modify Lab_Squirrels into Project 1

--- Scavenger-Hunt/Models/Task.swift
+++ Scavenger-Hunt/Models/Task.swift
@@ -1,9 +1,10 @@
 //
+// Modified
 //  Task.swift
 //  lab-task-squirrel
 //
 //  Created by Charlie Hieger on 11/15/22.
-//
+//  Modified by Syed Shah on 9/7/25
 
 import UIKit
 import CoreLocation
@@ -31,12 +32,14 @@ class Task {
 extension Task {
     static var mockedTasks: [Task] {
         return [
-            Task(title: "Pickup flowers from the North Beach Farmers Market 💐",
-                 description: "Try to get an arrangement with a fall vibe. The Urban Vergo floral truck has some great bouquets."),
-            Task(title: "Take Kingsley for a walk 🦮",
-                 description: "Bring treats to encourage him to walk, otherwise be prepared to carry him when he flops."),
-            Task(title: "Grab a fresh candle 🕯",
-                 description: "If you're by the Ferry Building, check out SOMA Small Batch Goods, they have the best candles EVER!!! Their California Morning candle is divine!")
+            Task(title: "Your Local Favorite Restaurant",
+                 description: "Where do you go for some good eats?"),
+            Task(title: "Your Favorite Local Cafe",
+                 description: "Where do you go to get energized for the day?"),
+            Task(title: "Your Favorite Beach",
+                 description: "What beach do you go to wind down and connect with the nature?"),
+            Task(title: "Your go-to Brunch Spot",
+                 description: "Your friends are in town, where are you taking them to eat?")
         ]
     }
 }
